0419b5f278aba145e307163ce0948e4fadd51131
nj <nickgentrymma@gmail.com>
2024-02-26 22:55:51 -0800

    added media mobile css, final mobile css touches

--- src/Components/Media/Media.jsx
+++ src/Components/Media/Media.jsx
@@ -37,3 +37,31 @@ function Media() {
     );
 };
 export default Media;
+
+
+    // const images = [
+    //     require("../../images/galleryPhotos/IMG_5429.jpg"),
+    //     require("../../images/galleryPhotos/IMG_5416.jpg"),
+    //     require("../../images/galleryPhotos/IMG_5442.jpg"),
+    //     require("../../images/galleryPhotos/IMG_0162.jpg"),
+    //     require("../../images/galleryPhotos/IMG_0162(1).jpg"),
+    //     require("../../images/galleryPhotos/IMG_0163.jpg"),
+    //     require("../../images/galleryPhotos/IMG_5414.jpg"),
+    //     require("../../images/galleryPhotos/IMG_5415.jpg"),
+    //     require("../../images/galleryPhotos/IMG_5418.jpg"),
+    //     require("../../images/galleryPhotos/IMG_5419.jpg"),
+    //     require("../../images/galleryPhotos/IMG_5427.jpg"),
+    //     require("../../images/galleryPhotos/IMG_5428.jpg"),
+    //     require("../../images/galleryPhotos/IMG_5426.jpg"),
+    //     require("../../images/galleryPhotos/IMG_5430.jpg"),
+    //     require("../../images/galleryPhotos/IMG_5431.jpg"),
+    //     require("../../images/galleryPhotos/IMG_5433.jpg"),
+    //     require("../../images/galleryPhotos/IMG_5434.jpg"),
+    //     require("../../images/galleryPhotos/IMG_5435.jpg"),
+    //     require("../../images/galleryPhotos/IMG_5436.jpg"),
+    //     require("../../images/galleryPhotos/IMG_5437.jpg"),
+    //     require("../../images/galleryPhotos/IMG_5439.jpg"),
+    //     require("../../images/galleryPhotos/IMG_5440.jpg"),
+    //     require("../../images/galleryPhotos/IMG_5441.jpg"),
+    //     require("../../images/galleryPhotos/IMG_5443.jpg")
+    // ];
