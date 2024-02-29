1eea10c35baf0eadaeb2c0a9e220805b213575d2
nj <nickgentrymma@gmail.com>
2024-02-29 05:41:05 -0800

    adjusted syntax format

--- src/Components/Media/Media.jsx
+++ src/Components/Media/Media.jsx
@@ -44,30 +44,96 @@ function Media() {
                 <h1 className="gallery-title">We Are LuvSikAngel</h1>
                 <div className="gallery-container">
                     <div className="first-row">
-                        <img className="gill" src={require("../../images/galleryPhotos/IMG_5429.jpg")} alt="LuvSikAngel" />
-                        <img className="gill" src={require("../../images/galleryPhotos/IMG_5416.jpg")} alt="LuvSikAngel" />
-                        <img className="gill" src={require("../../images/galleryPhotos/IMG_5442.jpg")} alt="LuvSikAngel" />
-                        <img className="gill" src={require("../../images/galleryPhotos/guitar.jpg")} alt="LuvSikAngel" />
+                        <img 
+                            className="gill" 
+                            src={require("../../images/galleryPhotos/IMG_5429.jpg")}
+                            alt="LuvSikAngel" />
+                        <img 
+                            className="gill" 
+                            src={require("../../images/galleryPhotos/IMG_5416.jpg")}
+                            alt="LuvSikAngel" />
+                        <img 
+                            className="gill" 
+                            src={require("../../images/galleryPhotos/IMG_5442.jpg")}
+                            alt="LuvSikAngel" />
+                        <img 
+                            className="gill" 
+                            src={require("../../images/galleryPhotos/guitar.jpg")} 
+                            alt="LuvSikAngel" />
                     </div>
                     <div className="second-row">
-                        <img className="gallery-image" src={require("../../images/galleryPhotos/IMG_0162.jpg")} alt="LuvSikAngel" />
-                        <img className="gallery-image" src={require("../../images/galleryPhotos/IMG_0162(1).jpg")} alt="LuvSikAngel" />
-                        <img className="gallery-image" src={require("../../images/galleryPhotos/IMG_0163.jpg")} alt="LuvSikAngel" />
-                        <img className="gallery-image" src={require("../../images/galleryPhotos/IMG_5414.jpg")} alt="LuvSikAngel" />
-                        <img className="gallery-image" src={require("../../images/galleryPhotos/IMG_5415.jpg")} alt="LuvSikAngel" />
-                        <img className="gallery-image" src={require("../../images/galleryPhotos/IMG_5418.jpg")} alt="LuvSikAngel" />
-                        <img className="gallery-image" src={require("../../images/galleryPhotos/IMG_5426.jpg")} alt="LuvSikAngel" />
-                        <img className="gallery-image" src={require("../../images/galleryPhotos/IMG_5430.jpg")} alt="LuvSikAngel" />
-                        <img className="gallery-image" src={require("../../images/galleryPhotos/IMG_5431.jpg")} alt="LuvSikAngel" />
-                        <img className="gallery-image" src={require("../../images/galleryPhotos/IMG_5433.jpg")} alt="LuvSikAngel" />
-                        <img className="gallery-image" src={require("../../images/galleryPhotos/IMG_5434.jpg")} alt="LuvSikAngel" />
-                        <img className="gallery-image" src={require("../../images/galleryPhotos/IMG_5435.jpg")} alt="LuvSikAngel" />
-                        <img className="gallery-image" src={require("../../images/galleryPhotos/IMG_5436.jpg")} alt="LuvSikAngel" />
-                        <img className="gallery-image" src={require("../../images/galleryPhotos/IMG_5437.jpg")} alt="LuvSikAngel" />
-                        <img className="gallery-image" src={require("../../images/galleryPhotos/IMG_5439.jpg")} alt="LuvSikAngel" />
-                        <img className="gallery-image" src={require("../../images/galleryPhotos/IMG_5440.jpg")} alt="LuvSikAngel" />
-                        <img className="gallery-image" src={require("../../images/galleryPhotos/IMG_5441.jpg")} alt="LuvSikAngel" />
-                        <img className="gallery-image" src={require("../../images/galleryPhotos/IMG_5443.jpg")} alt="LuvSikAngel" />
+                        <img 
+                            className="gallery-image" 
+                            src={require("../../images/galleryPhotos/IMG_0162.jpg")} 
+                            alt="LuvSikAngel" />
+                        <img 
+                            className="gallery-image" 
+                            src={require("../../images/galleryPhotos/IMG_0162(1).jpg")} 
+                            alt="LuvSikAngel" />
+                        <img 
+                            className="gallery-image" 
+                            src={require("../../images/galleryPhotos/IMG_0163.jpg")} 
+                            alt="LuvSikAngel" />
+                        <img 
+                            className="gallery-image" 
+                            src={require("../../images/galleryPhotos/IMG_5414.jpg")} 
+                            alt="LuvSikAngel" />
+                        <img 
+                            className="gallery-image" 
+                            src={require("../../images/galleryPhotos/IMG_5415.jpg")} 
+                            alt="LuvSikAngel" />
+                        <img 
+                            className="gallery-image" 
+                            src={require("../../images/galleryPhotos/IMG_5418.jpg")} 
+                            alt="LuvSikAngel" />
+                        <img 
+                            className="gallery-image" 
+                            src={require("../../images/galleryPhotos/IMG_5426.jpg")} 
+                            alt="LuvSikAngel" />
+                        <img 
+                            className="gallery-image" 
+                            src={require("../../images/galleryPhotos/IMG_5430.jpg")} 
+                            alt="LuvSikAngel" />
+                        <img 
+                            className="gallery-image" 
+                            src={require("../../images/galleryPhotos/IMG_5431.jpg")} 
+                            alt="LuvSikAngel" />
+                        <img 
+                            className="gallery-image" 
+                            src={require("../../images/galleryPhotos/IMG_5433.jpg")} 
+                            alt="LuvSikAngel" />
+                        <img 
+                            className="gallery-image" 
+                            src={require("../../images/galleryPhotos/IMG_5434.jpg")} 
+                            alt="LuvSikAngel" />
+                        <img 
+                            className="gallery-image" 
+                            src={require("../../images/galleryPhotos/IMG_5435.jpg")} 
+                            alt="LuvSikAngel" />
+                        <img 
+                            className="gallery-image" 
+                            src={require("../../images/galleryPhotos/IMG_5436.jpg")} 
+                            alt="LuvSikAngel" />
+                        <img 
+                            className="gallery-image" 
+                            src={require("../../images/galleryPhotos/IMG_5437.jpg")} 
+                            alt="LuvSikAngel" />
+                        <img 
+                            className="gallery-image" 
+                            src={require("../../images/galleryPhotos/IMG_5439.jpg")} 
+                            alt="LuvSikAngel" />
+                        <img 
+                            className="gallery-image" 
+                            src={require("../../images/galleryPhotos/IMG_5440.jpg")} 
+                            alt="LuvSikAngel" />
+                        <img 
+                            className="gallery-image" 
+                            src={require("../../images/galleryPhotos/IMG_5441.jpg")} 
+                            alt="LuvSikAngel" />
+                        <img 
+                            className="gallery-image" 
+                            src={require("../../images/galleryPhotos/IMG_5443.jpg")} 
+                            alt="LuvSikAngel" />
                     </div>
                 </div>
             </div>
