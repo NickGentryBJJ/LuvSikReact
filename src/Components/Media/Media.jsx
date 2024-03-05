ddeb6f81a7448bfd6eaba730549d1f11612626f2
nj <nickgentrymma@gmail.com>
2024-03-05 03:18:06 -0800

    Optimized Gallery images.

--- src/Components/Media/Media.jsx
+++ src/Components/Media/Media.jsx
@@ -2,11 +2,13 @@ import React, { useEffect } from "react";
 import './Media.css';
 
 function Media() {
+    const firstRowImgs = [require("../../images/galleryPhotos/IMG_5429.jpg"), require("../../images/galleryPhotos/IMG_5416.jpg"), require("../../images/galleryPhotos/IMG_5442.jpg"), require("../../images/galleryPhotos/guitar.jpg")]
+    const mainGalleryImgs = [require("../../images/galleryPhotos/IMG_0162.jpg"), require("../../images/galleryPhotos/IMG_0163.jpg"), require("../../images/galleryPhotos/IMG_5414.jpg"), require("../../images/galleryPhotos/IMG_5415.jpg"), require("../../images/galleryPhotos/IMG_5418.jpg"), require("../../images/galleryPhotos/IMG_5426.jpg"), require("../../images/galleryPhotos/IMG_5430.jpg"), require("../../images/galleryPhotos/IMG_5431.jpg"), require("../../images/galleryPhotos/IMG_5433.jpg"), require("../../images/galleryPhotos/IMG_5434.jpg"), require("../../images/galleryPhotos/IMG_5435.jpg"), require("../../images/galleryPhotos/IMG_5436.jpg"), require("../../images/galleryPhotos/IMG_5437.jpg"), require("../../images/galleryPhotos/IMG_5439.jpg"), require("../../images/galleryPhotos/IMG_5440.jpg"), require("../../images/galleryPhotos/IMG_5441.jpg"), require("../../images/galleryPhotos/IMG_5443.jpg"), require("../../images/galleryPhotos/IMG_0162.jpg")]        
+    
     useEffect(() => {
         const images = document.querySelectorAll('.gill');
         const imagen = document.querySelectorAll('.gallery-image');
         const cont = document.querySelector('.second-row');
-
         const fadeInImage = (image, index) => {
             setTimeout(() => {
                 image.classList.add('fade-in');
@@ -37,103 +39,30 @@ function Media() {
         };
         window.addEventListener('scroll', handleScroll);
     }, []);
-
     return (
         <div id="media-section" className="media-wrap">
             <div className="media-cont">
                 <h1 className="gallery-title">We Are LuvSikAngel</h1>
                 <div className="gallery-container">
                     <div className="first-row">
-                        <img 
-                            className="gill" 
-                            src={require("../../images/galleryPhotos/IMG_5429.jpg")}
-                            alt="LuvSikAngel" />
-                        <img 
-                            className="gill" 
-                            src={require("../../images/galleryPhotos/IMG_5416.jpg")}
-                            alt="LuvSikAngel" />
-                        <img 
-                            className="gill" 
-                            src={require("../../images/galleryPhotos/IMG_5442.jpg")}
-                            alt="LuvSikAngel" />
-                        <img 
-                            className="gill" 
-                            src={require("../../images/galleryPhotos/guitar.jpg")} 
-                            alt="LuvSikAngel" />
+                        {firstRowImgs.map((imgSrc, index) => (
+                            <img
+                                key={index}
+                                className="gill"
+                                src={imgSrc}
+                                alt="LuvSikAngel"
+                            />
+                        ))}
                     </div>
                     <div className="second-row">
-                        <img 
-                            className="gallery-image" 
-                            src={require("../../images/galleryPhotos/IMG_0162.jpg")} 
-                            alt="LuvSikAngel" />
-                        <img 
-                            className="gallery-image" 
-                            src={require("../../images/galleryPhotos/IMG_0163.jpg")} 
-                            alt="LuvSikAngel" />
-                        <img 
-                            className="gallery-image" 
-                            src={require("../../images/galleryPhotos/IMG_5414.jpg")} 
-                            alt="LuvSikAngel" />
-                        <img 
-                            className="gallery-image" 
-                            src={require("../../images/galleryPhotos/IMG_5415.jpg")} 
-                            alt="LuvSikAngel" />
-                        <img 
-                            className="gallery-image" 
-                            src={require("../../images/galleryPhotos/IMG_5418.jpg")} 
-                            alt="LuvSikAngel" />
-                        <img 
-                            className="gallery-image" 
-                            src={require("../../images/galleryPhotos/IMG_5426.jpg")} 
-                            alt="LuvSikAngel" />
-                        <img 
-                            className="gallery-image" 
-                            src={require("../../images/galleryPhotos/IMG_5430.jpg")} 
-                            alt="LuvSikAngel" />
-                        <img 
-                            className="gallery-image" 
-                            src={require("../../images/galleryPhotos/IMG_5431.jpg")} 
-                            alt="LuvSikAngel" />
-                        <img 
-                            className="gallery-image" 
-                            src={require("../../images/galleryPhotos/IMG_5433.jpg")} 
-                            alt="LuvSikAngel" />
-                        <img 
-                            className="gallery-image" 
-                            src={require("../../images/galleryPhotos/IMG_5434.jpg")} 
-                            alt="LuvSikAngel" />
-                        <img 
-                            className="gallery-image" 
-                            src={require("../../images/galleryPhotos/IMG_5435.jpg")} 
-                            alt="LuvSikAngel" />
-                        <img 
-                            className="gallery-image" 
-                            src={require("../../images/galleryPhotos/IMG_5436.jpg")} 
-                            alt="LuvSikAngel" />
-                        <img 
-                            className="gallery-image" 
-                            src={require("../../images/galleryPhotos/IMG_5437.jpg")} 
-                            alt="LuvSikAngel" />
-                        <img 
-                            className="gallery-image" 
-                            src={require("../../images/galleryPhotos/IMG_5439.jpg")} 
-                            alt="LuvSikAngel" />
-                        <img 
-                            className="gallery-image" 
-                            src={require("../../images/galleryPhotos/IMG_5440.jpg")} 
-                            alt="LuvSikAngel" />
-                        <img 
-                            className="gallery-image" 
-                            src={require("../../images/galleryPhotos/IMG_5441.jpg")} 
-                            alt="LuvSikAngel" />
-                        <img 
-                            className="gallery-image" 
-                            src={require("../../images/galleryPhotos/IMG_5443.jpg")} 
-                            alt="LuvSikAngel" />
-                        <img 
-                            className="gallery-image" 
-                            src={require("../../images/galleryPhotos/IMG_0162.jpg")} 
-                            alt="LuvSikAngel" />
+                        {mainGalleryImgs.map((imgSrc, index) => (
+                            <img
+                                key={index}
+                                className="gallery-image"
+                                src={imgSrc}
+                                alt="LuvSikAngel"
+                            />
+                        ))}
                     </div>
                 </div>
             </div>
