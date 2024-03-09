7f711bdbfd6ca7b0120f307c465acd9fcd99c200
nj <nickgentrymma@gmail.com>
2024-03-09 03:48:23 -0800

    fixed and added show dates

--- src/Components/Shows/ShowIndex/ShowIndex.jsx
+++ src/Components/Shows/ShowIndex/ShowIndex.jsx
@@ -8,7 +8,8 @@ function ShowIndex() {
         "FA": "Farrington's",
         "PSB": "Pine Street Bar",
         "PISS": "Pioneer Saloon",
-        "MHBC": "Mike Hess Brewing"
+        "MHBC": "Mike Hess Brewing",
+        "WI": "WiseGirl"
     };
     const dates = {
         "FA": {
@@ -17,10 +18,22 @@ function ShowIndex() {
             "time": "8:00PM - 12:00AM",
             "year": "2024"
         },
+        "CJ": {
+            "month": "April",
+            "day": "21st",
+            "time": "12:00PM",
+            "year": "2024"
+        },
         "PSB": {
             "month": "May",
             "day": "25th",
             "time": "9:00PM - 1:00AM",
+            "year": "2024"
+        },
+        "CJ2": {
+            "month": "June",
+            "day": "9th",
+            "time": "4:00PM - 8:00PM",
             "year": "2024"
         },
         "MHBC": {
@@ -29,10 +42,10 @@ function ShowIndex() {
             "time": "4:00pm - 8:00pm",
             "year": "2024"
         },
-        "MHBC2": {
-            "month": "June",
-            "day": "23rd",
-            "time": "5:00PM - 7:30PM",
+        "WI": {
+            "month": "July",
+            "day": "19th",
+            "time": "7:00pm - 10:00pm",
             "year": "2024"
         },
         "PISS": {
@@ -49,9 +62,11 @@ function ShowIndex() {
                     LuvSikAngel Live!
                 </h1>
                 <ShowIndexItem year ={dates["FA"].year} location={locations["FA"]} month={dates["FA"].month} day={dates["FA"].day} time={dates["FA"].time} />
+                <ShowIndexItem year ={dates["CJ"].year} location={locations["CJ"]} month={dates["CJ"].month} day={dates["CJ"].day} time={dates["CJ"].time} />
                 <ShowIndexItem year ={dates["PSB"].year} location={locations["PSB"]} month={dates["PSB"].month} day={dates["PSB"].day} time={dates["PSB"].time} />
+                <ShowIndexItem year ={dates["CJ2"].year} location={locations["CJ"]} month={dates["CJ2"].month} day={dates["CJ2"].day} time={dates["CJ2"].time} />
                 <ShowIndexItem year ={dates["MHBC"].year} location={locations["MHBC"]} month={dates["MHBC"].month} day={dates["MHBC"].day} time={dates["MHBC"].time} />
-                <ShowIndexItem year ={dates["MHBC2"].year} location={locations["MHBC"]} month={dates["MHBC2"].month} day={dates["MHBC2"].day} time={dates["MHBC2"].time} />
+                <ShowIndexItem year ={dates["WI"].year} location={locations["WI"]} month={dates["WI"].month} day={dates["WI"].day} time={dates["WI"].time} />
                 <ShowIndexItem year ={dates["PISS"].year} location={locations["PISS"]} month={dates["PISS"].month} day={dates["PISS"].day} time={dates["PISS"].time} />
             </div>
         </div>
--- src/Components/Shows/ShowIndexItem/ShowIndexItem.jsx
+++ src/Components/Shows/ShowIndexItem/ShowIndexItem.jsx
@@ -9,7 +9,8 @@ function ShowIndexItem(props) {
         "Farrington's": "https://www.google.com/maps?gs_lcrp=EgZjaHJvbWUqEggBEC4YChivARjHARiABBiOBTIGCAAQRRg5MhIIARAuGAoYrwEYxwEYgAQYjgUyDwgCEC4YChivARjHARiABDISCAMQLhgKGK8BGMcBGIAEGJgFMgkIBBAAGAoYgAQyCQgFEAAYChiABDIJCAYQABgKGIAEMg8IBxAuGAoYrwEYxwEYgAQyCQgIEAAYChiABNIBCDQxMjBqMGo0qAIAsAIA&um=1&ie=UTF-8&fb=1&gl=us&sa=X&geocode=KYdtcRw4YYWAMREdcjGHr53U&daddr=1938+Contra+Costa+Blvd,+Pleasant+Hill,+CA+94523",
         "Pine Street Bar": "https://www.google.com/maps?gs_lcrp=EgZjaHJvbWUqFQgCEC4YJxivARjHARiABBiKBRiOBTIGCAAQRRg5MgYIARBFGEAyFQgCEC4YJxivARjHARiABBiKBRiOBTIHCAMQABiABDINCAQQABiDARixAxiABDIKCAUQABixAxiABDIQCAYQLhivARjHARiABBiOBTINCAcQLhixAxiABBiKBdIBCDI0MThqMGo3qAIAsAIA&um=1&ie=UTF-8&fb=1&gl=us&sa=X&geocode=KVUqXV-354-AMSZenMcXDet2&daddr=875+Rincon+Ave,+Livermore,+CA+94551",
         "Pioneer Saloon": "https://www.google.com/maps/dir//Pioneer+Saloon,+2925+Woodside+Rd,+Woodside,+CA+94062/@37.4289167,-122.2529443,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x808fa4111018f733:0x76787400b7827090!2m2!1d-122.2503694!2d37.4289167!3e0?entry=ttu",
-        "Mike Hess Brewing": "https://www.google.com/maps/dir//2313+Oak+Grove+Rd,+Walnut+Creek,+CA+94598/@37.9286953,-122.0990693,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x8085605e0bf70651:0xcd2f8752e9eb7b38!2m2!1d-122.0166783!2d37.9287131?entry=ttu"
+        "Mike Hess Brewing": "https://www.google.com/maps/dir//2313+Oak+Grove+Rd,+Walnut+Creek,+CA+94598/@37.9286953,-122.0990693,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x8085605e0bf70651:0xcd2f8752e9eb7b38!2m2!1d-122.0166783!2d37.9287131?entry=ttu",
+        "WiseGirl": "https://www.google.com/maps/dir//1932+Oak+Park+Blvd,+Pleasant+Hill,+CA+94523/@37.93408,-122.1577401,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x8085616778c4755b:0x69d0da664a3e9e21!2m2!1d-122.07534!2d37.9340441?entry=ttu"
     };
     return (
         <div className="show-item-wrapper">
